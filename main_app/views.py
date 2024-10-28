2aeb0e1fed227930ed981e52825af0ad3ebb2d46
Banton65 <banton65@gmail.com>
2024-10-28 14:51:48 -0400

    django- cat-lecture

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Simulate some Cat data
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-# List of mock cats
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# # List of mock cats
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
+
+# views.py
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+    
 
 # View function for cat index
 def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
     return render(request, 'cats/index.html', {'cats': cats})
+
 
 def home(request):
     return render(request, 'home.html')
