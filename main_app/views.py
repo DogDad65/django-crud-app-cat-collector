cd299d7953cc3c6330f9e4364e02dac71c67f10b
Banton65 <banton65@gmail.com>
2024-10-29 18:51:09 -0400

    cat-app-one-to-many

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Cat
+from .forms import FeedingForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 # Simulate some Cat data
 # class Cat:
@@ -17,19 +22,50 @@ from .models import Cat
 #     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
 # ]
 
-# views.py
+# This expects a template in the format of 
+# templates/<app_name>/<model_name>_form.html
+# templates/main_app/cat_form.html
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+# Go to the models.py file for the cat model
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
-
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # include the cat and feeding_form in the context
+        'cat': cat, 'feeding_form': feeding_form
+    })
     
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
 
+  
 # View function for cat index
 def cat_index(request):
     cats = Cat.objects.all()  # look familiar?
     return render(request, 'cats/index.html', {'cats': cats})
-
 
 def home(request):
     return render(request, 'home.html')
